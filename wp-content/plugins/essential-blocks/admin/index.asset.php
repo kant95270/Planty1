2efc979641007743b58b8f15b1d20033d1b1343d
kribi karim <kant95270@gmail.com>
2023-11-28 09:30:49 +0100

    ajout page accueil

---
+++ wp-content/plugins/essential-blocks/admin/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-element', 'wp-hooks', 'wp-i18n'), 'version' => '7436ec9cfbebb4d72155');
